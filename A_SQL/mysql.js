0e19f5ec64f0f72bf3f38466bcdc0fccf52458cd
agaviriak <gaviria.alberto@gmail.com>
2022-05-15 20:40:43 -0500

    Mayo15

--- A_SQL/mysql.js
+++ A_SQL/mysql.js
@@ -15,13 +15,24 @@ connection.connect(function(error){
       console.log('Conexion correcta.');
    }
 });
-connection.query("select * from notas", function(error,rows){
-    console.log(rows),
+connection.query("select * from notas", function(error,resultado){
+    /*console.log(resultado),*/
     console.log('Errores en la consulta:  ' + error),
-    console.log("Total Registros: " + rows.length),
-    console.log(rows[6])
+    console.log("Total Registros: " + resultado.length),
+    console.log(resultado[6])
 
 });
 /*var insertar= "insert into notas (Nombre,Materia,Nota) values ('Lucas','Matematicas',4)";
-connection.query(insertar);*/
+connection.query(insertar,function(error,resultado){
+    console.log(error),
+    console.log("Registros Insertados :"+resultado.affectedRows)
+});*/
+process.stdout.write("Esta es otra manera de imprimir\n", function(){
+
+});
+process.stdin.on('data',function(data){
+    process.stdout.write(data.toString());
+});
+
+/*process.stdin.on('nombre',*/
 connection.end();
